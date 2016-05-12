1c621ece47fa49b3bcdec6cb9c5c83d57637b534
james.dunnam <jamesd1184@gmail.com>
2016-05-12 13:04:59 -0400

    fp ch-5 and peg game

---
+++ src/brave_and_true/functional_programming_ch5.clj
@@ -0,0 +1,33 @@
+(ns brave-and-true.functional-programming-ch5)
+
+;;Composing functions
+;;note -- * then inc
+((comp inc *) 2 3)
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+;; :attributes -> intelligence *remember keywords are functions too!
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(c-int character)
+
+;;when one of the functions you want to compose needs to take
+;;more than one argument
+(defn spell-slots
+  [char]
+  (int (inc (/ (c-int char) 2))))
+
+;;less noise with comp
+(def spell-slots-comp (comp int inc #(/ % 2) c-int))
+
+(= (spell-slots character)
+   (spell-slots-comp character))
+
+(comment (memoize)
+         "clojure has a memoize function!")
+
