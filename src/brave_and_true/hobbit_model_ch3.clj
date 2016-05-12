583e7d4cf2471652e5216110cdc81b38a00cf07b
james.dunnam <jamesd1184@gmail.com>
2016-05-12 11:41:49 -0400

    clojure for the brave and true: notes file and ch 3

---
+++ src/brave_and_true/hobbit_model_ch3.clj
@@ -0,0 +1,66 @@
+(ns brave-and-true.hobbit-model-ch3)
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+;;loop with recursion
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+;;with reduce - process all and build result
+(defn better-symmetrize-body-parts
+  "Expects same as above
+  reduce takes a fn w/ 2 params.
+  first is the collector and second the parts vec"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts
+          ))
+
+(defn hit
+  "The body part is hit when the accumulated sizes exceeds the target"
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)
+        log (println (str target))]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))
+        )
+      ))
+  )
---
+++ src/brave_and_true/notes.clj
@@ -0,0 +1,8 @@
+(ns brave-and-true.notes)
+
+;;Clojure returns last for eval'd
+(defn joe
+  []
+  (+ 1 304)
+  30
+  "joe")
