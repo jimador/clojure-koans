156bbf76e77ba4a979f36a486d2c9a839d3614e6
james.dunnam <jamesd1184@gmail.com>
2016-05-12 13:31:10 -0400

    orgainzation ch6 and cheese theivery

---
+++ src/brave_and_true/organization_ch6.clj
@@ -0,0 +1,21 @@
+(ns brave-and-true.organization-ch6)
+
+;;namespaces -- map to symbols
+;;symbols -- datatypes w/in the clojure language (map, inc, etc)
+;;refer -- gives you fine grained control over how you refer to objects
+;;         in other namespaces. When you call refer, you can also pass it
+;;         the filters :only, :exclude, and :rename. ex
+;;         (clojure.core/refer 'cheese.taxonomy :only ['bries])
+;;alias -- (clojure.core/alias 'taxonomy 'cheese.taxonomy)
+;;a more succienct ex is (require '[the-divine-cheese-code.visualization.svg :as svg])
+;;there are six references within ns
+;;(:refer-clojure)
+;;(:require) -- (:require the-divine-cheese-code.visualization.svg)) (notice, no '
+;;              also, can have multiple in [thing.stuff :as do] [thing.more :as foo]
+;;(:use) -- (:use [clojure.java browse io])
+;;          equivalent to (use 'clojure.java.browse)
+;;                        (use 'clojure.java.io)
+;;(:import)
+;;(:load)
+;;(:gen-class)
+
---
+++ src/brave_and_true/the_devine_cheese_code/README.md
@@ -0,0 +1,7 @@
+# the-divine-cheese-code
+
+clojure practice for http://www.braveclojure.com/
+
+## Usage
+
+    $ java -jar the-divine-cheese-code-0.1.0-standalone.jar [args]
